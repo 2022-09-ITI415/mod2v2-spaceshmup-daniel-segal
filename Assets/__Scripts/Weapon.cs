e6324e8fe6b05d591fe93671d59e673f8896f87d
DSegalStudent <dan.segal359@gmail.com>
2022-10-13 20:00:48 -0400

    added comment on where to put laser weapon in weapon scrpt

--- Assets/__Scripts/Weapon.cs
+++ Assets/__Scripts/Weapon.cs
@@ -132,6 +132,8 @@ public class Weapon : MonoBehaviour {
                 p.transform.rotation = Quaternion.AngleAxis(-10, Vector3.back);
                 p.rigid.velocity = p.transform.rotation * vel;
                 break;
+            //case WeaponType.laser:
+
         }
     }
 
